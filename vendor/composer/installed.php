b56964f87f6ef1e8123d9a1e6b19c282ce8396f3
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2022-04-07 11:01:52 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'klansot/new_composer_autoload',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'klansot/new_composer_autoload' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
